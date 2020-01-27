9406cf5e748b1beb85e63908e118eea5df16517d
Hunter_LC <lc411887055@gmail.com>
2020-01-27 16:09:47 +0800

    first

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, url_for, request, redirect, session, make_response, jsonify, render_template
+from flask_cors import *
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('start.html')
+
+@app.route('/detect')
+def detect_ui():
+    return render_template('detection.html')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    print("前端正在请求登录...")
+    if request.method == 'POST':
+        print('用户名'+request.form['user'])
+        print('密码'+request.form['password'])
+        if request.form['user'] == 'admin' and request.form['password'] == '123456':
+            print('成功了')
+            return render_template("success.html")
+        else:
+            print('错误了')
+            return 'ERROR'
+
+if __name__ == '__main__':
+    app.run(debug=True)
